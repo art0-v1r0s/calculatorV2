c4f03ffba85422efeeb7d8661ea0b006b6021163
art0v1r0S <arthurd2000@gmail.com>
2021-09-09 14:46:51 +0200

    v1 calculator

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC = gcc									# Nom du compilateur
+CFLAGS = -std=c99 -Wall -Wextra -Werror -Wstrict-prototypes -fanalyzer		# Les flags
+RM = rm -f 									# Commande pour supprimer des fichiers
+NAME = calculator									# Nom de l'exécutable
+SRC = exo1.c	 								# Les fichiers source. Soir *.c, soit on liste les fichiers
+EXEC = ./
+
+all: compilation execute 
+
+execute:
+	$(EXEC)$(NAME)
+
+compilation:							# Règle all, exécutée par défaut avec la commande "make"
+	$(CC) $(CFLAGS) $(SRC) -o $(NAME)			# On n'utilise que des variables ici. On modifie les variables, pas la ligne de commande
+
+clean:								# Règle clean, pour supprimer les fichiers générés par le compilateur. On utilise "make clean"
+	$(RM) $(NAME)
+
+re: clean all							# "make re", pour supprimer les anciens fichiers et les génére à nouveau.
---
+++ exo1.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long long factorielle(unsigned long long a)
+{
+    unsigned long long r = 1;
+
+    for (unsigned long long i = 2; i <= a; ++i)
+        r *= i;
+
+    return r;
+}
+
+int division(int a , int b)
+{
+
+/*
+	if ( b = 0 )
+	{
+	
+		printf("impossible to divise by 0");
+		exit(1);
+	}
+*/	
+	int q = 0;
+	int r = a;
+	while ( r >= b )
+	{
+	
+		q = q + 1;
+		r = r - b;
+	}
+	printf("ur quotient is %d & the rest is %d\n",q,r);
+
+	return 0 ;
+}
+
+int main (int argc, char **argv )
+{
+
+	int result;
+
+	printf("Welcome to the Calculator\n");
+	if ( argc < 3 )
+	{
+	
+		printf("Usage : ./exo1 a operator b\n");
+		exit(1);
+	}
+	printf("%c",argv[2][0]);
+	
+	switch (argv[2][0])
+	{
+
+		case '!' :
+                        result = factorielle(atoi(argv[1]));
+                        printf("votre resultat :  %d\n",result);
+                        break;
+
+	
+		case '*' :
+			result = atoi(argv[1])*atoi(argv[3]);
+			printf("votre resultat :  %d\n",result);
+			break;
+
+		case 'x' :
+			result = atoi(argv[1])*atoi(argv[3]);
+			printf("votre resultat :  %d\n",result);
+			break;
+		
+		case '+' :
+                        result = atoi(argv[1])+atoi(argv[3]);
+                        printf("votre resultat :  %d\n",result);
+                        break;
+	
+		case '-' :	
+			result = atoi(argv[1])-atoi(argv[3]);
+			printf("votre resultat :  %d\n",result);
+			break;	
+		case '/' :	
+			result = division(atoi(argv[1]),atoi(argv[3]));
+			break;
+
+		case '%' :
+			result = division(atoi(argv[1]),atoi(argv[3]));
+			break;
+
+		default :
+			printf("Error! Operator is not correct\n");	
+	}
+
+	return 0;
+
+}
